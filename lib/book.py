5f4619996a112901b37e42881fc904296121e962
Dara16 <just4oludee@yahoo.com>
2023-05-02 18:52:32 -0400

    tests passed

--- lib/book.py
+++ lib/book.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count=0):
+        self.title = title
+        self.page_count = page_count
+
+    def get_page_count(self):
+        return self._page_count
+
+    def set_page_count(self, num):    
+        if isinstance(num, int):
+            self._page_count = num
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    page_count = property(get_page_count, set_page_count)
+
+towers = Book("Towering Inferno", 417)
+towers.genre = "Non-fiction"
+print(towers.genre)
+print(towers.page_count)
+print(towers.title)
